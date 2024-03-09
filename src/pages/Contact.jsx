6ed2c5359845d9e67a2f88dc7a72994f5e24a684
Rab Lap <rahulbushi69@gmail.com>
2024-03-09 13:10:30 +0530

    Latest Build File

--- src/pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import TalkToUs from "../sections/ContactSections/TalkToUs";
-
-export default function Contact() {
-    return (
-        <div className='mt-5'>
-            <TalkToUs />
-        </div>
-    )
-}
